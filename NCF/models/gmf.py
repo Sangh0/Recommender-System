767a1e8c38e32886914bdf71ef1d0d0ab6ea652d
Sangh0 <hop7311@naver.com>
2023-09-03 01:35:18 +0900

    update: weight initialization with normal dist

--- NCF/models/gmf.py
+++ NCF/models/gmf.py
@@ -4,7 +4,12 @@ import torch.nn as nn
 
 class GMF(nn.Module):
 
-    def __init__(self, num_users: int, num_items: int, latent_dim: int=64):
+    def __init__(
+        self, 
+        num_users: int, 
+        num_items: int, 
+        latent_dim: int=16,
+    ):
         super(GMF, self).__init__()
 
         self.embedding_user = nn.Embedding(num_embeddings=num_users, embedding_dim=latent_dim)
@@ -13,6 +18,8 @@ class GMF(nn.Module):
         self.fc = nn.Linear(in_features=latent_dim, out_features=1)
         self.logistic = nn.Sigmoid()
 
+        self._init_weights_()
+
     def forward(self, user_indices, item_indices):
         user_embedding = self.embedding_user(user_indices)
         item_embedding = self.embedding_item(item_indices)
@@ -20,3 +27,8 @@ class GMF(nn.Module):
         logits = self.fc(element_product)
         rating = self.logistic(logits)
         return rating
+    
+    def _init_weights_(self):
+        for name, module in self.named_modules():
+            if isinstance(module, nn.Linear):
+                nn.init.normal(module.weight, mean=0.0, std=0.01)
